9999ec300e5e7806383f579d1975de09fc3cc335
BhanuGawshinde <bhanugawshinde@gmail.com>
2025-07-30 23:38:11 +0530

    path parameter.

--- rest-api/src/main/java/com/rest/api/controller/Controller.java
+++ rest-api/src/main/java/com/rest/api/controller/Controller.java
@@ -1,10 +1,15 @@
 package com.rest.api.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.websocket.server.PathParam;
+import lombok.experimental.PackagePrivate;
 
 @RestController
 public class Controller {
@@ -13,5 +18,17 @@ public class Controller {
 	public ResponseEntity<String> getCourse(@RequestParam("cname") String cname, @RequestParam("tname") String tname) {
 		String respBody = cname + " by " + tname + " Fee is 7770 RS.";
 		return new ResponseEntity<>(respBody, HttpStatus.OK);
+	}
+	
+	@GetMapping("/book/{bname}")
+	public ResponseEntity<String> getBookPrice(@PathVariable String bname){
+		String respBody = bname + " price is 400 RS.";
+		return new ResponseEntity<>(respBody, HttpStatus.OK);
+	}
+	
+	@GetMapping("/book/name/{bname}/tname/{tname}")
+	public ResponseEntity<String> getBook(@PathVariable("bname") String bookName, @PathVariable("tname") String trainerName){
+		String respBody = bookName + " by " + trainerName + " is out of stock.";
+		return new ResponseEntity<String>(respBody, HttpStatus.OK);
 	}
 }
