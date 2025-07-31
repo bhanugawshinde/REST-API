124a959d480cc3f0ca0afe0b850a30c448941996
BhanuGawshinde <bhanugawshinde@gmail.com>
2025-07-31 22:47:30 +0530

    produces.

---
+++ rest-api/src/main/java/com/rest/api/controller/ProductRestController.java
@@ -0,0 +1,36 @@
+package com.rest.api.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rest.api.been.Product;
+
+@RestController
+public class ProductRestController {
+
+	@GetMapping(
+			value = "/product",
+			produces = {
+				"application/json",
+				"application/xml"
+			}
+	)
+	public ResponseEntity<Product> getProduct(){
+		Product p = new Product(1, "name", 400);
+		return new ResponseEntity<>(p, HttpStatus.OK);
+	}
+	
+	@GetMapping("/products")
+	public ResponseEntity<List<Product>> getproducts(){
+		Product p1 = new Product(1, "p1", 10);
+		Product p2 = new Product(2, "p2", 20);
+		Product p3 = new Product(3, "p3", 30);
+		
+		return new ResponseEntity<>(List.of(p1,p2,p3), HttpStatus.OK);
+	}
+	
+}
